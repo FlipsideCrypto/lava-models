e26f0fa70543b37e8aaca968d494a59ea2fe3965
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-06-17 22:45:10 -0400

    batches

--- models/testnet/streamline/silver/core/realtime/streamline__testnet_blocks_realtime.sql
+++ models/testnet/streamline/silver/core/realtime/streamline__testnet_blocks_realtime.sql
@@ -4,7 +4,7 @@
         func = 'streamline.udf_bulk_rest_api_v2',
         target = "{{this.schema}}.{{this.identifier}}",
         params ={ "external_table" :"testnet_blocks",
-        "sql_limit" :"1000",
+        "sql_limit" :"500",
         "producer_batch_size" :"50",
         "worker_batch_size" :"25",
         "sql_source" :"{{this.identifier}}" }
--- models/testnet/streamline/silver/core/realtime/streamline__testnet_transactions_realtime.sql
+++ models/testnet/streamline/silver/core/realtime/streamline__testnet_transactions_realtime.sql
@@ -4,7 +4,7 @@
         func = 'streamline.udf_bulk_rest_api_v2',
         target = "{{this.schema}}.{{this.identifier}}",
         params ={ "external_table" :"testnet_tx_search",
-        "sql_limit" :"10000",
+        "sql_limit" :"500",
         "producer_batch_size" :"50",
         "worker_batch_size" :"25",
         "exploded_key": "[\"result.txs\"]",
