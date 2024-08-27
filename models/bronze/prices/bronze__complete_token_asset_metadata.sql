ca4e9ac8dc9273896e715301886e238e92a06cd9
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-08-27 17:02:15 -0400

    prices

---
+++ models/bronze/prices/bronze__complete_token_asset_metadata.sql
@@ -0,0 +1,28 @@
+{{ config (
+    materialized = 'view'
+) }}
+
+SELECT
+    'ulava' AS token_address,
+    asset_id,
+    symbol,
+    'Lava Network' AS NAME,
+    decimals,
+    'lava' AS blockchain,
+    'lava' AS blockchain_name,
+    blockchain_id,
+    is_deprecated,
+    provider,
+    source,
+    _inserted_timestamp,
+    inserted_timestamp,
+    modified_timestamp,
+    complete_token_asset_metadata_id,
+    _invocation_id
+FROM
+    {{ source(
+        'crosschain_silver',
+        'complete_token_asset_metadata'
+    ) }}
+WHERE
+    asset_id = 'lava-network'
---
+++ models/bronze/prices/bronze__complete_token_prices.sql
@@ -0,0 +1,31 @@
+{{ config (
+    materialized = 'view'
+) }}
+
+SELECT
+    HOUR,
+    'ulava' AS token_address,
+    asset_id,
+    symbol,
+    'Lava Network' AS NAME,
+    decimals,
+    price,
+    'lava' AS blockchain,
+    'lava' AS blockchain_name,
+    blockchain_id,
+    is_imputed,
+    is_deprecated,
+    provider,
+    source,
+    _inserted_timestamp,
+    inserted_timestamp,
+    modified_timestamp,
+    complete_token_prices_id,
+    _invocation_id
+FROM
+    {{ source(
+        'crosschain_silver',
+        'complete_token_prices'
+    ) }}
+WHERE
+    asset_id = 'lava-network'
